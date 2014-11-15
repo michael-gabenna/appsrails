ec61c1730aa0f6afcfcf15844601f1684843b24b
diegobiavati <diegobiavati@gmail.com>
2014-11-15 22:16:44 +0000

    Renew BD

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115205549) do
-
-  create_table "commits", force: true do |t|
-    t.string   "name"
-    t.text     "commit"
-    t.integer  "posts_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20141110223605) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
